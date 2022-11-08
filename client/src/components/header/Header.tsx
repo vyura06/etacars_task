6668951468fc39993d825a2dc39cffb1a5787eac
vyura06 <vechorko.yura@gmail.com>
2022-11-08 17:40:42 +0300

    Update header and header styles

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import { float } from "../../float/float";
+import { useAppSelector } from "../../hooks/hooks";
+import { calcCurrentWallet, calcInitialWallet } from "../../calculation/calc";
+import { WalletModal } from "../wallet/walletModal";
 
-export const Header = (): JSX.Element => {
+export const Header = () => {
+  const [modalActive, setModalActive] = useState<boolean>(false);
+  const currencies = useAppSelector(
+    (state) => state.currencyReducer.currencies
+  );
+  const { currencies: walletCurrency } = useAppSelector(
+    (state) => state.walletReducer
+  );
+  const topCurrencies = currencies.slice(0, 3);
+
+  const currentPrice = calcCurrentWallet(walletCurrency, currencies);
+  const initialPrice = calcInitialWallet(walletCurrency);
+  const walletDifference = currentPrice - initialPrice;
+  const walletDifferencePercent = initialPrice
+    ? (walletDifference / initialPrice) * 100
+    : 0;
+
   return (
     <header className="header">
-      <div className="main">
-        <div>222</div>
+      <ul className="top-currency">
+        {topCurrencies.map((e) => (
+          <li className="top-currency__item" key={e.id}>
+            {e.name} - ${float(e.priceUsd)}
+          </li>
+        ))}
+      </ul>
+      <div className="header__wallet" onClick={() => setModalActive(true)}>
+        <div className="header__wallet-info">
+          <div>${float(currentPrice)}</div>
+          <div>
+            {walletDifference > 0 ? "+" : ""}
+            {float(walletDifference)}$
+          </div>
+          <div>{float(walletDifferencePercent)}%</div>
+        </div>
       </div>
+      {modalActive && (
+        <WalletModal
+          walletCurrency={walletCurrency}
+          setActive={setModalActive}
+        />
+      )}
     </header>
   );
 };
