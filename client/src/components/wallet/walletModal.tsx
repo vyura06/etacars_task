9dee7f10b390cac92ff31cdd8e163dd5a0dff046
vyura06 <vechorko.yura@gmail.com>
2022-11-08 17:43:53 +0300

    Create walletModal and styles

---
+++ client/src/components/wallet/walletModal.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Wallet } from "../../interfaces/Wallet";
+import { float } from "../../float/float";
+import { useAppDispatch } from "../../hooks/hooks";
+import { deleteCurrencyFromWallet } from "../../redux/slices/walletSlice";
+
+interface WalletModalProps {
+  setActive: (option: boolean) => void;
+  walletCurrency: Wallet[];
+}
+
+export const WalletModal = ({
+  setActive,
+  walletCurrency,
+}: WalletModalProps) => {
+  const dispatch = useAppDispatch();
+  return (
+    <div className="modal" onClick={() => setActive(false)}>
+      <div className="modal__content" onClick={(e) => e.stopPropagation()}>
+        <p style={{ fontSize:"2rem", fontWeight:"bold"}}>Your wallet</p>
+        <ul style={{ textAlign: "left"}} className="wallet-list">
+          {walletCurrency.map((e) => (
+            <li style={{ marginBottom: "1rem"}} className="wallet-list__item" key={e.id}>
+              <a style={{ marginLeft: "1rem"}}>{e.name}</a>
+              <a style={{ marginLeft: "1rem"}}>${float(e.price)}</a>
+              <a style={{ marginLeft: "1rem"}}>{e.count}</a>
+              <a 
+                style={{ marginLeft: "1rem"}}
+                className="delete-button"
+                onClick={() => dispatch(deleteCurrencyFromWallet(e.id))}
+              >
+                Delete
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
